261566b888f725c3d400ff97e5ad97a6e92b6c48
AnicetChiza <chizaanicet@gmail.com>
2024-03-05 16:43:04 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,10 @@
 const icon = document.querySelector('.menu');
-const navItems = document.querySelector('nav');
+const navItems = document.querySelector('ul');
+const exitIcon = document.querySelector('.exit');
 
 if (icon) {
     icon.addEventListener('click', () => {
         navItems.classList.add('active');
+        exitIcon.classList.remove('active');
     })
 }
